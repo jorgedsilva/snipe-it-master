70d5baf0cc7ab2122013200591fde0bba55f4db5
jorgedsilva <jorgedireitosilva@gmail.com>
2019-02-08 19:34:12 +0000

    Initial commit

---
+++ config/version.php
@@ -0,0 +1,10 @@
+<?php
+return array (
+  'app_version' => 'v4.6.8',
+  'full_app_version' => 'v4.6.8 - build 3959-g194d0733d',
+  'build_version' => '3959',
+  'prerelease_version' => '',
+  'hash_version' => 'g194d0733d',
+  'full_hash' => 'v4.6.8-14-g194d0733d',
+  'branch' => 'master',
+);
